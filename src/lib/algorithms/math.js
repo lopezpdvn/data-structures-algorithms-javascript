0bca431059968c609a7bc3e17c4a893535d30435
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-03-25 03:31:44 -0600

    Implemented/tested `algorithms.math.fibonacciSeq` with generator

--- src/lib/algorithms/math.js
+++ src/lib/algorithms/math.js
@@ -1,8 +1,10 @@
 ﻿'use strict';
 
 function* fibonacciSeq(n) {
+    if(n < 1) return;
+    yield 0;
     let prev = 0, curr = 1, i = -1, tmp;
-    while(++i < n) {
+    while(++i < n - 1) {
         yield curr;
         tmp = curr;
         curr += prev;
--- test/lib/algorithms/math.js
+++ test/lib/algorithms/math.js
@@ -4,9 +4,13 @@ const assert = require('assert');
 const path = require('path');
 
 const dsalg = require('../../..');
+const fibonacciSeq = dsalg.algorithms.math.fibonacciSeq;
 
 describe('Fibonacci numbers', () => {
     it('With generators', () => {
-        assert.ok(true);
+        assert.deepStrictEqual([...fibonacciSeq(5)], [0, 1, 1, 2, 3]);
+        assert.deepStrictEqual([...fibonacciSeq(17)],
+                [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
+                987]);
     });
 });
