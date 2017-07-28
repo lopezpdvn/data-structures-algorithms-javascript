a44220613d0dfa6429edcc16beb553712aed4b6e
Pedro Ivan Lopez <m@pedroivanlopez.com>
2017-07-28 18:28:15 -0500

    `DirectedGraphAdjacencyList`: Implemented/tested `depthFirstTraversalIterative`

--- src/lib/data_structures/graph/directed_graph_adjacency_list.js
+++ src/lib/data_structures/graph/directed_graph_adjacency_list.js
@@ -14,18 +14,51 @@ class DirectedGraphAdjacencyList {
         this.nodes.push(node);
         this.count++;
     }
+
+    flagNodesUnvisited() {
+        this.flagNodes(State.unvisited);
+    }
+
+    flagNodes(state = State.unvisited) {
+        this.nodes.forEach(node => node.state = state);
+    }
+
+    static *depthFirstTraversalIterative(node, stack = []) {
+        if(!node)
+            return;
+        stack.push(node);
+        while(stack.length) {
+            node = stack.pop();
+            if(node.state == State.visited)
+                continue;
+            yield node;
+            node.state = State.visited;
+            for(let i of node.adjacentReverse()) {
+                stack.push(i);
+            }
+        }
+    }
 }
 
 class Node {
     constructor(vertex = null, linkedList = []) {
         this.vertex = vertex;
         this.adjacent = [];
-        this.count = 0;
         this.state = 0;
     }
 
     addAdjacent(...nodes) {
         this.adjacent = this.adjacent.concat(nodes);
+    }
+
+    *adjacentReverse() {
+        for(let i = this.adjacent.length - 1; i >= 0; i--) {
+            yield this.adjacent[i];
+        }
+    }
+
+    [Symbol.iterator]() {
+        return this.adjacent[Symbol.iterator]();
     }
 }
 
--- test/lib/data_structures/graph/directed_graph_adjacency_list.js
+++ test/lib/data_structures/graph/directed_graph_adjacency_list.js
@@ -10,7 +10,8 @@ const Node = directed_graph_adjacency_list.Node;
 const State = directed_graph_adjacency_list.State;
 
 describe('Directed Graph Adjacency List', function() {
-    let dirGraph = undefined;
+    const graph0 = {};
+    const graph0Graph = new DirectedGraphAdjacencyList();
     const A = new Node('A');
     const B = new Node('B');
     const C = new Node('C');
@@ -35,9 +36,63 @@ describe('Directed Graph Adjacency List', function() {
     J.addAdjacent(I);
     //K.addAdjacent();
 
+    nodes.forEach(x => {
+        graph0Graph.add(x)
+        const nodeKey = x.vertex;
+        graph0[nodeKey] = { node: x };
+    });
+
+    graph0['graph'] = graph0Graph;
+    graph0['A']['dft-str'] = 'ABCDHEKFJI';
+    graph0['B']['dft-str'] = 'BACDHEKFJI';
+    graph0['C']['dft-str'] = 'CDHEABJIFK';
+    graph0['D']['dft-str'] = 'DHEABCFJIK';
+    graph0['E']['dft-str'] = 'EABCDHKFJI';
+    graph0['F']['dft-str'] = 'F';
+    graph0['G']['dft-str'] = 'GDHEABCFJIK';
+    graph0['H']['dft-str'] = 'HDEABCFJIK';
+    graph0['I']['dft-str'] = 'IF';
+    graph0['J']['dft-str'] = 'JIF';
+    graph0['K']['dft-str'] = 'K';
+
+    graph0['A']['bft-str'] = 'ABCJDFIHEK';
+    graph0['B']['bft-str'] = 'BACJDFIHEK';
+    graph0['C']['bft-str'] = 'CDFHEKABJI';
+    graph0['D']['bft-str'] = 'DHEKABCJFI';
+    graph0['E']['bft-str'] = 'EADBHCJKFI';
+    graph0['F']['bft-str'] = 'F';
+    graph0['G']['bft-str'] = 'GDIHFEKABCJ';
+    graph0['H']['bft-str'] = 'HDEKABCJFI';
+    graph0['I']['bft-str'] = 'IF';
+    graph0['J']['bft-str'] = 'JIF';
+    graph0['K']['bft-str'] = 'K';
+
+    const graph1Graph = new DirectedGraphAdjacencyList();
+    const graph1 = { graph: graph1Graph };
+    const nodeKeys = [...'ABCDEFGHIJK'];
+
+    function traversalTest(traversalAlgorithm, traversalType) {
+
+        nodeKeys.forEach(nodeKey => {
+            graph0Graph.flagNodesUnvisited();
+            let str = '';
+            const startNode = graph0[nodeKey]['node'];
+            const traversalSequence = graph0[nodeKey][traversalType];
+
+            [...traversalAlgorithm(startNode)].forEach(
+                node => str += node.vertex);
+
+            assert.strictEqual(str, traversalSequence);
+        });
+    }
+
     it('Instantiation', function() {
-        dirGraph = new DirectedGraphAdjacencyList();
-        nodes.forEach(x => dirGraph.add(x));
-        assert.ok(dirGraph instanceof DirectedGraphAdjacencyList);
+        const graph0Graph = new DirectedGraphAdjacencyList();
+        assert.ok(graph0Graph instanceof DirectedGraphAdjacencyList);
+    });
+
+    it('Depth First Traversal iterative', function() {
+        traversalTest(graph0Graph.constructor.depthFirstTraversalIterative,
+            'dft-str');
     });
 });
