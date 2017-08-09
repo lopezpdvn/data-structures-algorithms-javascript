60aa3b49f16f70d1552fbe9dd2d8b7af6c4c6277
Pedro Ivan Lopez <m@pedroivanlopez.com>
2017-08-08 21:24:40 -0500

    Setup array sorting tests

--- test/lib/data_structures/array/sorted_array.js
+++ test/lib/data_structures/array/sorted_array.js
@@ -7,6 +7,37 @@ const dsalg = require('../../../..');
 const SortedArrayNumber = dsalg.data_structures.array.sorted_array.SortedArrayNumber;
 
 describe('Sorted Array', function() {
+    const sortedBig = [0, 111, 222, 333, 444, 555, 666,777, 888, 999];
+    const sortedTwo = [222, 888];
+    const sortedOne = [999];
+    const sortedThree = [0, 333, 999];
+    const sortedFour = [111, 444, 555, 666];
+    const sortedEmpty = [];
+    const numSorted = [sortedOne, sortedTwo, sortedThree, sortedFour,
+        sortedBig, sortedEmpty];
+
+    let unsortedBig, unsortedTwo, unsortedOne, unsortedThree, unsortedFour,
+        unsortedEmpty, numUnsorted;
+
+    beforeEach(function() {
+        unsortedBig = [888, 222, 333, 0, 999, 777, 555, 111, 666, 444];
+        unsortedTwo = [888, 222];
+        unsortedOne = [999];
+        unsortedThree = [333, 0, 999];
+        unsortedFour = [555, 111, 666, 444];
+        unsortedEmpty = [];
+        numUnsorted = [unsortedOne, unsortedTwo, unsortedThree, unsortedFour,
+            unsortedBig, unsortedEmpty];
+    });
+
+    const numArraySortingTest = sortingAlg => {
+        numSorted.map((sortedArr, i) => {
+            const unsortedArr = numUnsorted[i];
+            sortingAlg(unsortedArr, unsortedArr.length);
+            assert.deepEqual(unsortedArr, sortedArr);
+        });
+    }
+
     it('Instantiation', function() {
         const sa = new SortedArrayNumber();
         assert.ok(sa instanceof SortedArrayNumber);
@@ -41,5 +72,9 @@ describe('Sorted Array', function() {
         const maxLen = sortedArr.length;
         const a = new SortedArrayNumber(maxLen);
         assert.strictEqual(a.count, 0);
+    });
+
+    it('Bubble Sort', function() {
+        numArraySortingTest(SortedArrayNumber.BubbleSort);
     });
 });
